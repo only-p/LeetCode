77824a06cd37463d943adf7c3374f28f2a86455c
only-p <pramodpk360@gmail.com>
2024-07-20 21:33:00 +0530

    Time: 7 ms (93.46%) | Memory: 13.2 MB (8.20%) - LeetSync

--- 16-3sum-closest/3sum-closest.cpp
+++ 16-3sum-closest/3sum-closest.cpp
@@ -5,8 +5,8 @@ public:
         sort(nums.begin(), nums.end());
         int closest = nums[0] + nums[1] + nums[2];
         for (int i = 0; i < nums.size() - 2; i++) {
-            if (i > 0 && nums[i] == nums[i - 1])
-                continue;
+            // if (i > 0 && nums[i] == nums[i - 1])
+            //     continue;
 
             int left = i + 1;
             int right = nums.size() - 1;
