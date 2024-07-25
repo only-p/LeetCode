33703817eb48cbcca53d2a531038aa7f56f1a86f
only-p <pramodpk360@gmail.com>
2024-07-25 20:14:49 +0530

    Time: 8 ms (76.92%) | Memory: 12.2 MB (39.77%) - LeetSync

--- 3-longest-substring-without-repeating-characters/longest-substring-without-repeating-characters.cpp
+++ 3-longest-substring-without-repeating-characters/longest-substring-without-repeating-characters.cpp
@@ -6,7 +6,6 @@ public:
         unordered_map<char,int>frq;
         for(int end =0;end<s.size();end++){
             char r = s[end];
-            cout<<r;
             if(frq.find(r)!=frq.end()){
                 start = max(start,frq[r]+1);
             }
