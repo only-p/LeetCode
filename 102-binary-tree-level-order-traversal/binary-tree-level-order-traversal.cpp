51cc93949731e1b199d96faa04c1631f6ff4820f
only-p <pramodpk360@gmail.com>
2024-08-11 10:33:41 +0530

    Time: 5 ms (47.53%) | Memory: 15.2 MB (35.68%) - LeetSync

--- 102-binary-tree-level-order-traversal/binary-tree-level-order-traversal.cpp
+++ 102-binary-tree-level-order-traversal/binary-tree-level-order-traversal.cpp
@@ -6,21 +6,34 @@
  *     TreeNode *right;
  *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
  *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
- *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left),
+ * right(right) {}
  * };
  */
 class Solution {
-    void levelOrder(TreeNode * root,vector<vector<int>>&ans,int level){
-        if(root==NULL)return;
-        if(level>=ans.size())ans.push_back({});
-        ans[level].push_back(root->val);
-        levelOrder(root->left,ans,level+1);
-        levelOrder(root->right,ans,level+1);
-    }
 public:
     vector<vector<int>> levelOrder(TreeNode* root) {
-        vector<vector<int>>ans;
-        levelOrder(root,ans,0);
+
+        vector<vector<int>> ans;
+        if (root == NULL)
+            return ans;
+
+        queue<TreeNode*> pq;
+        pq.push(root);
+        while (!pq.empty()) {
+            int levelsize = pq.size();
+            vector<int> temp;
+            for (int i = 0; i < levelsize; i++) {
+                TreeNode* node = pq.front();
+                pq.pop();
+                temp.push_back(node->val);
+                if (node->left)
+                    pq.push(node->left);
+                if (node->right)
+                    pq.push(node->right);
+            }
+            ans.push_back(temp);
+        }
         return ans;
     }
 };
