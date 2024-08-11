bfc8ead661fdfd0157afd4f1a2c728959a9db4de
only-p <pramodpk360@gmail.com>
2024-08-11 21:42:23 +0530

    Time: 8 ms (93.44%) | Memory: 18 MB (58.67%) - LeetSync

---
+++ 437-path-sum-iii/path-sum-iii.cpp
@@ -0,0 +1,49 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left),
+ * right(right) {}
+ * };
+ */
+class Solution {
+    void countPathsRecursive(TreeNode* currentNode, int S,
+                             vector<int>& currentPath, int& ans) {
+        if (currentNode == nullptr)
+            return;
+
+        // Add the current node to the path
+        currentPath.push_back(currentNode->val);
+        long pathSum = 0;
+
+        // Find the sums of all sub-paths in the current path list
+        for (auto it = currentPath.rbegin(); it != currentPath.rend(); ++it) {
+            pathSum += *it;
+            // If the sum of any sub-path is equal to 'S', increment the answer
+            // count
+            if (pathSum == S) {
+                ans++;
+            }
+        }
+
+        // Traverse the left sub-tree
+        countPathsRecursive(currentNode->left, S, currentPath, ans);
+        // Traverse the right sub-tree
+        countPathsRecursive(currentNode->right, S, currentPath, ans);
+
+        // Remove the current node from the path to backtrack
+        currentPath.pop_back();
+    }
+
+public:
+    int pathSum(TreeNode* root, int targetSum) {
+        vector<int> currentPath;
+        int ans = 0;
+        countPathsRecursive(root, targetSum, currentPath, ans);
+        return ans;
+    }
+};
