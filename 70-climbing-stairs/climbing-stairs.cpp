cf25c86762f9651a4bf5c11d4f204fc7037b7aa0
only-p <pramodpk360@gmail.com>
2024-10-14 21:34:41 +0530

    Time: 0 ms (100.00%) | Memory: 7.7 MB (9.14%) - LeetSync

--- 70-climbing-stairs/climbing-stairs.cpp
+++ 70-climbing-stairs/climbing-stairs.cpp
@@ -10,10 +10,20 @@ private:
 
 public:
     int climbStairs(int n) {
-        vector<int>dp(n+1,-1);
-        return sol(n,dp);
+        vector<int> dp(n + 1, -1);
+        // return sol(n,dp);
         // if (n <= 2)
         //     return n;
         // return climbStairs(n - 1) + climbStairs(n - 2);
+
+        if (n <= 2)
+            return n;
+        dp[0] = 0;
+        dp[1] = 1;
+        dp[2] = 2;
+        for (int i = 3; i <= n; i++) {
+            dp[i] = dp[i - 1] + dp[i - 2];
+        }
+        return dp[n];
     }
 };
