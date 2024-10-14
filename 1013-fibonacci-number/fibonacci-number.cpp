68b42ab05e5d8bd466c70c03d9beb32f0b0f1c76
only-p <pramodpk360@gmail.com>
2024-10-14 21:24:40 +0530

    Time: 0 ms (100.00%) | Memory: 7.3 MB (90.27%) - LeetSync

--- 1013-fibonacci-number/fibonacci-number.cpp
+++ 1013-fibonacci-number/fibonacci-number.cpp
@@ -10,13 +10,22 @@ private:
 
 public:
     int fib(int n) {
-        vector<int> dp(n + 1, -1);
+        // vector<int> dp(n + 1, -1);
+        // if(n<2)return n;
+        // dp[0] = 0;
+        // dp[1] = 1;
+        // for (int i = 2; i <= n; i++) {
+        //     dp[i] = dp[i - 1] + dp[i - 2];
+        // }
+        // return dp[n];
         if(n<2)return n;
-        dp[0] = 0;
-        dp[1] = 1;
-        for (int i = 2; i <= n; i++) {
-            dp[i] = dp[i - 1] + dp[i - 2];
+        int prev1=0,prev2=1;
+        int current=0;
+        for(int i=2;i<=n;i++){
+            current = prev1+prev2;
+            prev1=prev2;
+            prev2=current;
         }
-        return dp[n];
+        return current;
     }
 };
