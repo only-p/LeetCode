4a7987978b03b777604b330a117e480eeb4a0da2
only-p <pramodpk360@gmail.com>
2024-10-14 21:20:31 +0530

    Time: 0 ms (100.00%) | Memory: 7.6 MB (12.54%) - LeetSync

--- 1013-fibonacci-number/fibonacci-number.cpp
+++ 1013-fibonacci-number/fibonacci-number.cpp
@@ -1,14 +1,22 @@
 class Solution {
-    private:
-    int sol(int n, vector<int>&dp){
-        if(n<2)return n;
-        if(dp[n]!=-1)return dp[n];
-        return dp[n]=sol(n-1,dp)+sol(n-2,dp);
-    }
+private:
+    // int sol(int n, vector<int>& dp) {
+    //     if (n < 2)
+    //         return n;
+    //     if (dp[n] != -1)
+    //         return dp[n];
+    //     return dp[n] = sol(n - 1, dp) + sol(n - 2, dp);
+    // }
+
 public:
     int fib(int n) {
-       vector<int>dp(n+1,-1);
-       return sol(n,dp);
-        
+        vector<int> dp(n + 1, -1);
+        if(n<2)return n;
+        dp[0] = 0;
+        dp[1] = 1;
+        for (int i = 2; i <= n; i++) {
+            dp[i] = dp[i - 1] + dp[i - 2];
+        }
+        return dp[n];
     }
 };
