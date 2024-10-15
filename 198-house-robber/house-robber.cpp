688a50f22f5d4eb064931cbb678bff39d81a612a
only-p <pramodpk360@gmail.com>
2024-10-15 20:51:30 +0530

    Time: 0 ms (100.00%) | Memory: 9.5 MB (45.67%) - LeetSync

--- 198-house-robber/house-robber.cpp
+++ 198-house-robber/house-robber.cpp
@@ -3,6 +3,7 @@ private:
     int solve(vector<int>& nums, vector<int>& dp, int idx) {
         if (idx < 0)
             return 0;
+        if(idx==0)return nums[0];
         if (dp[idx] != -1)
             return dp[idx];
         int left = solve(nums, dp, idx - 2) + nums[idx];
@@ -14,5 +15,10 @@ public:
     int rob(vector<int>& nums) {
         vector<int> dp(nums.size() + 1, -1);
         return solve(nums, dp, nums.size() - 1);
+        // dp[0]=nums[0];
+        // for (int i = 1; i < nums.size(); i++) {
+            
+        // }
+        // return dp[nums.size() - 1];
     }
 };
