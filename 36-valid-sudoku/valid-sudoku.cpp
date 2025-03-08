35785ad3d2e3050f9b1aa6e2958c01df1479c583
Pramod Kumar <pramodpk360@gmail.com>
2025-03-08 19:53:02 +0530

    Time: 3 ms (63.06%) | Memory: 23.9 MB (52.34%) - LeetSync

--- 36-valid-sudoku/valid-sudoku.cpp
+++ 36-valid-sudoku/valid-sudoku.cpp
@@ -1,39 +1,4 @@
 class Solution {
-private:
-    bool row(int x, int y, vector<vector<char>>& board,
-             unordered_map<char, int> m) {
-        for (int i = x; i < 9; i++) {
-            if (m[board[i][y]] == 0)
-                return false;
-            m[board[i][y]]--;
-        }
-        return true;
-    }
-    bool col(int x, int y, vector<vector<char>>& board,
-             unordered_map<char, int> m) {
-        for (int i = y; i < 9; i++) {
-            if (m[board[x][i]] == 0)
-                return false;
-            m[board[x][i]]--;
-        }
-        return true;
-    }
-    bool mat(int x, int y, vector<vector<char>>& board,
-             unordered_map<char, int> m) {
-        for (int i = x; i < x + 3; i++) {
-            if (i >= 9)
-                continue;
-            for (int j = y; j < y + 3; j++) {
-                if (j >= 9)
-                    continue;
-                cout<<i<<'-'<<"-"<<m[board[i][j]];
-                if (m[board[i][j]] == 0)
-                    return false;
-                m[board[i][j]]--;
-            }
-        }
-        return true;
-    }
 
 public:
     bool isValidSudoku(vector<vector<char>>& board) {
@@ -81,6 +46,5 @@ public:
         }
 
         return true;
-    
     }
 };
