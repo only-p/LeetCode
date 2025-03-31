fc88efc16dd3603bd936dd2aa541b2759bfe9a21
Pramod Kumar <pramodpk360@gmail.com>
2025-03-31 12:59:37 +0530

    Time: 1564 ms (5.26%) | Memory: 102.8 MB (91.47%) - LeetSync

--- 739-daily-temperatures/daily-temperatures.cpp
+++ 739-daily-temperatures/daily-temperatures.cpp
@@ -1,17 +1,43 @@
 class Solution {
 public:
     vector<int> dailyTemperatures(vector<int>& temperatures) {
+        // vector<int> ans(temperatures.size(), 0);
+        // stack<int> s;
+        // // s.push(temperatures[temperatures.size() - 1]);
+        // for (int i = temperatures.size() - 1; i >= 0; i--) {
+        //     while (!s.empty() && temperatures[s.top()] <= temperatures[i]) {
+        //         s.pop();
+        //     }
+        //     if (!s.empty()) {
+        //         ans[i] = s.top() - i;
+        //     }
+        //     s.push(i);
+        // }
+        // return ans;
         vector<int> ans(temperatures.size(), 0);
         stack<int> s;
         // s.push(temperatures[temperatures.size() - 1]);
         for (int i = temperatures.size() - 1; i >= 0; i--) {
-            while (!s.empty() && temperatures[s.top()] <= temperatures[i]) {
-                s.pop();
+            if (s.empty()) {
+
+                s.push(i);
+            } else {
+                cout << s.top() << " ";
+                if (temperatures[s.top()] > temperatures[i]) {
+                    s.push(i);
+                    ans[i] = 1;
+
+                } else {
+                    while (!s.empty() &&
+                           temperatures[s.top()] <= temperatures[i]) {
+                        s.pop();
+                    }
+                    if (!s.empty()) {
+                        ans[i] = s.top() - i;
+                    }
+                    s.push(i);
+                }
             }
-            if (!s.empty()) {
-                ans[i] = s.top() - i;
-            }
-            s.push(i);
         }
         return ans;
     }
