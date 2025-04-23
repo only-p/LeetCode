610b7fc53284f9e42261f2be0381eca2e2b8a7be
Pramod Kumar <pramodpk360@gmail.com>
2025-04-23 19:25:47 +0530

    Time: 71 ms (52.36%) | Memory: 73.5 MB (11.63%) - LeetSync

--- 215-kth-largest-element-in-an-array/kth-largest-element-in-an-array.js
+++ 215-kth-largest-element-in-an-array/kth-largest-element-in-an-array.js
@@ -4,83 +4,75 @@
  * @return {number}
  */
 class MinHeap {
-    constructor(){
-        this.heap = [];
-    }
-    push(val){
-        this.heap.push(val);
-        this.bubbleUp();
-    }
-    pop(){
-        const max = this.heap[0];
-        const end = this.heap.pop();
-        if(this.heap.length>0){
-            this.heap[0] = end;
-            this.bubbleDown();
+  constructor() {
+    this.heap = [];
+  }
 
-        }
-        return max;
+  push(val) {
+    this.heap.push(val);
+    this._heapifyUp();
+  }
+
+  pop() {
+    if (this.heap.length === 0) return null;
+    const min = this.heap[0];
+    const end = this.heap.pop();
+    if (this.heap.length > 0) {
+      this.heap[0] = end;
+      this._heapifyDown();
     }
-    peek(){
-        return this.heap[0];
+    return min;
+  }
+
+  _heapifyUp() {
+    let idx = this.heap.length - 1;
+    while (idx > 0) {
+      let parentIdx = Math.floor((idx - 1) / 2);
+      if (this.heap[idx] >= this.heap[parentIdx]) break;
+      [this.heap[idx], this.heap[parentIdx]] = [this.heap[parentIdx], this.heap[idx]];
+      idx = parentIdx;
     }
-    bubbleUp(){
-        let idx = this.heap.length-1;
-        const element = this.heap[idx];
-        while(idx>0){
-            let parentIdx = Math.floor((idx-1)/2);
-            let parent = this.heap[parentIdx];
-            if(element>=parent)break;
-            this.heap[parentIdx] = element;
-            this.heap[idx] = parent;
-            idx= parentIdx;
-        }
+  }
+
+  _heapifyDown() {
+    let idx = 0;
+    const length = this.heap.length;
+    while (true) {
+      let left = 2 * idx + 1;
+      let right = 2 * idx + 2;
+      let smallest = idx;
+
+      if (left < length && this.heap[left] < this.heap[smallest]) {
+        smallest = left;
+      }
+      if (right < length && this.heap[right] < this.heap[smallest]) {
+        smallest = right;
+      }
+      if (smallest === idx) break;
+      [this.heap[idx], this.heap[smallest]] = [this.heap[smallest], this.heap[idx]];
+      idx = smallest;
     }
-    bubbleDown() {
-        let idx = 0;
-        const length = this.heap.length;
-        const element = this.heap[0];
-        while (true) {
-            let leftChildIdx = 2 * idx + 1;
-            let rightChildIdx = 2 * idx + 2;
-            let leftChild, rightChild;
-            let swap = null;
-            if (leftChildIdx < length) {
-                leftChild = this.heap[leftChildIdx];
-                if (leftChild < element) {
-                    swap = leftChildIdx;
-                }
-            }
-            if (rightChildIdx < length) {
-                rightChild = this.heap[rightChildIdx];
-                if (
-                    (swap === null && rightChild < element) || 
-                    (swap !== null && rightChild < leftChild)
-                ) {
-                    swap = rightChildIdx;
-                }
-            }
-            if (swap === null) break;
-            this.heap[idx] = this.heap[swap];
-            this.heap[swap] = element;
-            idx = swap;
-        }
-    }
+  }
+
+  size() {
+    return this.heap.length;
+  }
+
+  peek() {
+    return this.heap[0];
+  }
 }
 
 
-var findKthLargest = function(nums, k) {
-    
-   let heap = new MinHeap();
-    for (let i = 0; i < k; i++) {
-        heap.push(nums[i]);
-    }
-    for (let i = k; i < nums.length; i++) {
-        if (nums[i] > heap.peek()) {
-            heap.pop();
-            heap.push(nums[i]);
+var findKthLargest = function (nums, k) {
+
+    const minHeap = new MinHeap();
+    for (let num of nums) {
+        minHeap.push(num);
+        if (minHeap.size() > k) {
+            minHeap.pop();
         }
     }
-    return heap.peek();
-    
+    return minHeap.peek();
+
 };
