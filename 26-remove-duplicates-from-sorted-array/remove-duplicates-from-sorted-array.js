6852c4f0c72376cec904cf1fe8412225195118c9
Pramod Kumar <pramodpk360@gmail.com>
2025-05-19 23:29:47 +0530

    Time: 76 ms (5.26%) | Memory: 64.5 MB (5.03%) - LeetSync

---
+++ 26-remove-duplicates-from-sorted-array/remove-duplicates-from-sorted-array.js
@@ -0,0 +1,20 @@
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var removeDuplicates = function (nums) {
+    let i = 0;
+    let cnt = 0;
+    for (let j = 1; j < nums.length; j++) {
+        if (nums[i] !== nums[j]) {
+            console.log(nums[i + 1], nums[j]);
+            [nums[i + 1], nums[j]] = [nums[j], nums[i + 1]];
+            cnt++;
+            i++;
+        } 
+
+    }
+    return cnt+1;
+
+
+};
