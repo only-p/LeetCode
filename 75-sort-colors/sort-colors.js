74e3650fa05e4d7651c5d0f8a1c2fd3417739403
Pramod Kumar <pramodpk360@gmail.com>
2025-05-20 01:22:47 +0530

    Time: 68 ms (5.85%) | Memory: 64.8 MB (5.42%) - LeetSync

---
+++ 75-sort-colors/sort-colors.js
@@ -0,0 +1,25 @@
+/**
+ * @param {number[]} nums
+ * @return {void} Do not return anything, modify nums in-place instead.
+ */
+var sortColors = function (nums) {
+    let left = 0;
+    let right = nums.length - 1;
+    let i = 0;
+    while (i <= right) {
+        if (nums[i] == 2) {
+            [nums[right], nums[i]] = [nums[i], nums[right]];
+            right--;
+        }
+        else if (nums[i] == 0) {
+            [nums[left], nums[i]] = [nums[i], nums[left]];
+            left++;
+            i++;
+        }
+        else{
+            i++;
+        }
+        console.log(nums)
+    }
+
+};
