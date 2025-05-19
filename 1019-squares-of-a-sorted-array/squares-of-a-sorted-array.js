3761f7c92a0c56cab6b7acc32ec0d301ba8b5e16
Pramod Kumar <pramodpk360@gmail.com>
2025-05-19 23:49:20 +0530

    Time: 4 ms (72.48%) | Memory: 63.4 MB (5.37%) - LeetSync

--- 1019-squares-of-a-sorted-array/squares-of-a-sorted-array.js
+++ 1019-squares-of-a-sorted-array/squares-of-a-sorted-array.js
@@ -3,8 +3,21 @@
  * @return {number[]}
  */
 var sortedSquares = function (nums) {
-    nums = nums.map(val => val * val);
-    nums.sort((a, b) => a - b);
-    return nums
+    const ans = [];
+    let i = 0;
+    while (i < nums.length && nums[i] < 0) i++;
+    let j = i - 1;
+    while (j >= 0 && i < nums.length) {
+        if (Math.abs(nums[j]) > nums[i]) {
+            ans.push(nums[i] * nums[i]);
+            i++;
+        } else {
+            ans.push(nums[j] * nums[j])
+            j--;
+        }
+    }
+    while (j >= 0) {ans.push(nums[j] * nums[j--])}
+    while (i < nums.length) ans.push(nums[i] * nums[i++]);
+    return ans;
 
 };
