dfc213be3ff661fbbc9694f64fe55761fc093b18
Pramod Kumar <pramodpk360@gmail.com>
2026-04-03 16:05:48 +0530

    Time: 0 ms (100.00%) | Memory: 20.9 MB (64.97%) - LeetSync

--- 113-path-sum-ii/path-sum-ii.cpp
+++ 113-path-sum-ii/path-sum-ii.cpp
@@ -6,32 +6,26 @@
  *     TreeNode *right;
  *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
  *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
- *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left),
- * right(right) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
  * };
  */
 class Solution {
-    void dfs(TreeNode* root, int sum, vector<int>& currentPath,
-             vector<vector<int>>& ans) {
-        if (!root)
-            return;
-        currentPath.push_back(root->val);
+    void solve(TreeNode* root,vector<vector<int>>&ans,vector<int>&path,int target){
+        if(root==NULL)return;
+        target -= root->val;
+        path.push_back(root->val);
+        if(!root->left && !root->right && target==0)ans.push_back(path);
+        solve(root->left,ans,path,target);
+        solve(root->right,ans,path,target);
+        path.pop_back();
 
-        if (sum == root->val && !root->left && !root->right) {
-            ans.push_back(currentPath);
-
-        } else {
-            dfs(root->left, sum - root->val, currentPath, ans);
-            dfs(root->right, sum - root->val, currentPath, ans);
-        }
-        currentPath.pop_back();
     }
-
 public:
     vector<vector<int>> pathSum(TreeNode* root, int targetSum) {
-        vector<vector<int>> ans;
-        vector<int> currentPath;
-        dfs(root, targetSum, currentPath, ans);
+        vector<vector<int>>ans;
+        vector<int>path;
+        solve(root,ans,path,targetSum);
         return ans;
+        
     }
 };
